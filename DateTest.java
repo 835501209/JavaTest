846a3da9c91c67f1f56c5bb2c9f5d2e8d60115ed
unknown <you@example.com>
2019-11-30 16:48:17 +0800

    正则表达式、日期

--- DateTest.java
+++ DateTest.java
@@ -7,9 +7,34 @@ public class DateTest {
 
     }
 
-    public static void main(String args[])  {
-        // Date date = new Date();
+    public void Interval() {
+        try {
+            // Date a = new Date();
+            long start = System.currentTimeMillis();
+            System.out.println("start:" + start);
+            Thread.sleep(1000 * 3); // 休眠3秒 //java中除了RuntimeException及其任何子类,其它类都被java的异常强制处理机制强制异常处理
+            // Date b = new Date();
+            long end = System.currentTimeMillis();
+            System.out.println("end:" + end);
+            System.out.println(end - start);
+        } catch (Exception e) {
 
+        }
+    }
+
+    public void Calendars(){
+        //创建Calendar类对象
+        Calendar c1 = Calendar.getInstance();    //默认是当前时间的对象
+        // c.set(2015,9,7);
+        // c.set(Calendar.YEAR,2016);
+        c1.add(Calendar.DATE,-10);
+        System.out.println("c:"+c1.getTime());
+    }
+
+    // public final void set(int year,int month,int day);
+    public static void main(String args[]) {
+        // Date date = new Date();
+        DateTest time = new DateTest();
         // System.out.println(date.toString());
         // // 获取距离1970年的毫秒数
         // System.out.println(date.getTime());
@@ -31,13 +56,16 @@ public class DateTest {
         // System.out.printf("HH:MM:SS格式（24时制）：%tT%n", date);
         // // R的使用
         // System.out.printf("HH:MM格式（24时制）：%tR", date);
-        try{
-            System.out.println(new Date() + "\n");
-            Thread.sleep(1000 * 3); // 休眠3秒      //java中除了RuntimeException及其任何子类,其它类都被java的异常强制处理机制强制异常处理
-            System.out.println(new Date() + "\n");
-        }
-        catch(Exception e){
-            
-        }
+        // try{
+        // System.out.println(new Date() + "\n");
+        // Thread.sleep(1000 * 3); // 休眠3秒
+        // //java中除了RuntimeException及其任何子类,其它类都被java的异常强制处理机制强制异常处理
+        // System.out.println(new Date() + "\n");
+        // }
+        // catch(Exception e){
+
+        // }
+        // time.Interval();
+        time.Calendars();
     }
 }
--- Zhengze.java
+++ Zhengze.java
@@ -3,7 +3,13 @@ import java.util.regex.Pattern;
 
 public class Zhengze {
     String keyword = "akun";
-    String str = "我是akun，我爱chang、tiao、200rap";
+    String strs = "我是akun，我爱chang、tiao、200rap";
+    private static final String reg = "\\bakun\\b";
+    private static final String str = "akun is ikun,akun just akun,akun is not akun";
+
+    String reg2 = "foo";
+    String str2 = "fooooooofoooo";
+    String str3 = "ooooooooofooo";
 
     public Zhengze() {
         // boolean isinclude = Pattern.matches(keyword, str);
@@ -11,28 +17,55 @@ public class Zhengze {
         System.out.println(Pattern.matches(keyword, str));
     }
 
-    public static void main(String[] args) {
-        System.out.println("启动");
-        Zhengze zz = new Zhengze();
-        zz.groupBox();
-    }
-
     public void groupBox() {
         String pattern = "(\\D*)(\\d+)(.*)";
         // 创建Pattern对象
         Pattern r = Pattern.compile(pattern);
-          System.out.println("r: " + r );
-          System.out.println("pattern: " + pattern );
+        System.out.println("r: " + r);
+        System.out.println("pattern: " + pattern);
         // 创建matcher对象
         // Matcher m = r.matcher(this.str);
-        Matcher m = r.matcher(this.str);
-        if (m.find( )) {
-            System.out.println("Found value: " + m.group(0) );
-            System.out.println("Found value: " + m.group(1) );
-            System.out.println("Found value: " + m.group(2) );
-            System.out.println("Found value: " + m.group(3) ); 
-         } else {
+        Matcher m = r.matcher(this.strs);
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+            System.out.println("Found value: " + m.group(1));
+            System.out.println("Found value: " + m.group(2));
+            System.out.println("Found value: " + m.group(3));
+        } else {
             System.out.println("NO MATCH");
-         }
+        }
+    }
+     public void startEnd(){
+        Pattern p =  Pattern.compile(reg);
+        Matcher m = p.matcher(str);
+        int count = 0;
+        while(m.find()){
+            System.out.println("count"+count);
+            System.out.println("start"+m.start());
+            System.out.println("end"+m.end());
+            count++;
+        }
+     }
+
+     public void matchers(){
+        Pattern p =  Pattern.compile(reg2);
+        Matcher m1 = p.matcher(str2);
+        Matcher m2 = p.matcher(str3);
+        System.out.println("1:"+m1.matches());
+        System.out.println("2:"+m2.matches());
+        System.out.println("1:"+m1.lookingAt());
+        System.out.println("2:"+m2.lookingAt());
+        String sks = "dog";
+        // String res = m1.replaceFirst(sks);
+        String res = m1.replaceAll(sks);
+        System.out.println("res:"+res);
+     }
+
+    public static void main(String[] args) {
+        System.out.println("启动");
+        Zhengze zz = new Zhengze();
+        // zz.groupBox();
+        // zz.startEnd();
+        zz.matchers();
     }
 }
