68a2fd57907434d65c3d673cc7e4b10aca3914ce
guoyang <835501209@qq.com>
2020-06-18 17:43:23 +0800

    数据结构

---
+++ packageTea/teaMac.java
@@ -0,0 +1,10 @@
+package packageTea;
+
+public class teaMac {
+  public static void main(String aegs[]) {
+    System.out.print("茶包");
+  }
+  public void drinkTea(){
+    System.out.print("喝茶");
+  }
+}
---
+++ packageTest.java
@@ -0,0 +1,10 @@
+import packageTea.*;
+
+public class packageTest {
+  public static void main(String args[]) {
+    teaMac a = new teaMac();
+    teaMac b = new teaMac();
+    a.drinkTea();
+    b.drinkTea();
+  }
+}
